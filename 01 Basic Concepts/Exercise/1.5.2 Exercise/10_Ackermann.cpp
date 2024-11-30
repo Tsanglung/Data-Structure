bbb3c491cdb543e0fac66bc764fea2b65c0ea51f
Tsanglung <ab1726531@gmail.com>
2024-11-30 08:53:32 +0800

    Ch 1.5.2 Exercises 10. ackermann's function

---
+++ 01 Basic Concepts/Exercise/1.5.2 Exercise/10_Ackermann.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//Ackermann's function
+//Book p.33 Exercises 10.
+int Ackermann(int m,int n)
+{   //recursive
+    if(m == 0) return n + 1; 
+    else if(n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+ 
+int main()
+{
+    int m , n;
+    cout << "Input m , n : ";
+    cin >> m >> n;
+    cout<< "Ackermann(" << m << "," << n <<") = " << Ackermann(m , n) << endl; 
+}
