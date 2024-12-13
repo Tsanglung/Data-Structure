c2bbef00d1cd93d327a16a08a46c0ef81d40a7b3
Tsanglung <ab1726531@gmail.com>
2024-12-13 22:01:40 +0800

    Stack define

---
+++ 03 Stack and Queue/06 Expressions/Stack.h
@@ -0,0 +1,23 @@
+#ifndef Stack_H
+#define Stack_H
+
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Stack
+{
+    private:
+        T *stack;   //array for stack elements
+        int top;    //top ¤¸¯À
+        int capacity;   //capacity of stack array
+    public:
+        Stack(int);  //constructor
+        bool IsEmpty() const;   //stack empty ? true | false
+        T& Top()const;  //return top
+        void Push(const T&);  //push e into stack
+        void Pop(); //delete top
+        void Output();
+};
+
+#endif
